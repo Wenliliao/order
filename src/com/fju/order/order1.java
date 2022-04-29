02841f46b87a4fd8e416737b8abf3f3b5c11833f
Wenliliao <wenliliao0823@gmail.com>
2022-04-29 16:05:35 +0800

    order

---
+++ src/com/fju/order/order1.java
@@ -0,0 +1,12 @@
+package com.fju.order;
+
+public class order1 {
+    delivery = ("sevenEleven,Family,Hilife,Shopee");
+    order = ("700,250,550,220,130");
+    shipFeed =("60,60,50,19,19");
+    if{
+        shopee order >= 190;
+        shopFeed = 0;
+    }
+
+}
